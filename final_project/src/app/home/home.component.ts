a3d27ba7ff0524e726f7afc5ae8781427bc4293d
autorejecttop <autorejecttop@outlook.com>
2025-04-16 10:45:28 +0700

    Hero section done

--- final_project/src/app/home/home.component.ts
+++ final_project/src/app/home/home.component.ts
@@ -1,11 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
