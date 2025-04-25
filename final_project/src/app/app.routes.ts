004e23c4fe72187fe7728aa49e2d42b27a24bea0
autorejecttop <autorejecttop@outlook.com>
2025-04-25 20:36:57 +0700

    Initial contact page

--- final_project/src/app/app.routes.ts
+++ final_project/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     title: 'About | Mr. AI',
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    title: 'Contact | Mr. AI',
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
