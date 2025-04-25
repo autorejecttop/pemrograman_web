4e7892ea51f71cb17173cae3f8ec657c28793cc9
autorejecttop <autorejecttop@outlook.com>
2025-04-25 20:28:35 +0700

    initial about page

--- final_project/src/app/app.routes.ts
+++ final_project/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {
     title: 'Home | Mr. AI',
     path: '',
     component: HomeComponent,
+  },
+  {
+    title: 'About | Mr. AI',
+    path: 'about',
+    component: AboutComponent,
   },
 ];
