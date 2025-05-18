e0be87d12be608761a9dbd6b7455f0dbffa12147
autorejecttop <autorejecttop@outlook.com>
2025-05-18 14:34:34 +0700

    Added create new user form UI

--- dalpa/src/app/users/users-form/users-form.component.ts
+++ dalpa/src/app/users/users-form/users-form.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-users-form',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './users-form.component.html',
-  styleUrl: './users-form.component.scss'
+  styleUrl: './users-form.component.scss',
 })
-export class UsersFormComponent {
-
-}
+export class UsersFormComponent {}
