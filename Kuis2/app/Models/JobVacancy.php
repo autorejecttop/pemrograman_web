9a470e5dd7b0851d8b9a4b326b82ceb523fbfbd9
autorejecttop <autorejecttop@outlook.com>
2025-07-02 15:23:16 +0700

    Kuis2

---
+++ Kuis2/app/Models/JobVacancy.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use App\Models\AvailablePosition;
+use Illuminate\Database\Eloquent\Model;
+
+class JobVacancy extends Model
+{
+    protected $fillable = ['job_category_id', 'company', 'address', 'description'];
+
+    // Relasi ke Available Positions
+    public function availablePositions()
+    {
+        return $this->hasMany(AvailablePosition::class);
+    }
+}
